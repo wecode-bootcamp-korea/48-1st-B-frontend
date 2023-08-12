10f94e7361cadff5f3c7084c542d362ed48658fb
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-12 18:41:59 +0900

    Add : 레이아웃 완성

--- src/pages/PostAdd/PostAdd.js
+++ src/pages/PostAdd/PostAdd.js
@@ -1,14 +1,26 @@
-import React from "react";
-import "./PostAdd.scss";
+import React from 'react';
+import './PostAdd.scss';
 
 const PostAdd = () => {
-
-return(
-  <div className="postAdd">
-    <div>로그인</div>
-  </div>
-);
-
+  return (
+    <div>
+      <div className="userBox">
+        <div className="userWrap">
+          <img className="user" src="/images/user.png" />
+          <div className="contentBox">
+            <div className="userName">NAME</div>
+            <textarea className="textArea" name="content" cols="80" rows="30">
+              게시물
+            </textarea>
+            <div className="buttonWrap">
+              <button className="cancelButton">취소</button>
+              <button className="addButton">게시</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostAdd;
--- src/pages/PostEditing/PostEditing.js
+++ src/pages/PostEditing/PostEditing.js
@@ -1,14 +1,31 @@
-import React from "react";
-import "./PostEditing.scss";
+import React from 'react';
+import './PostEditing.scss';
 
 const PostEditing = () => {
-
-return(
-  <div className="postEditing">
-    <div>로그인</div>
-  </div>
-);
-
+  return (
+    <div>
+      <div className="commentUserBox">
+        <div className="commentUserWrap">
+          <img className="commentUser" src="/images/user.png" />
+          <div className="commentContentBox">
+            <div className="commentUserName">NAME</div>
+            <textarea
+              className="commentTextArea"
+              name="content"
+              cols="80"
+              rows="30"
+            >
+              내용 수정하기
+            </textarea>
+            <div className="commentButtonWrap">
+              <button className="commentCancelButton">취소</button>
+              <button className="commentAddButton">게시</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostEditing;
