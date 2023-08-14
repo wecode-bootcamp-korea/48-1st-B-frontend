fb9c0b85f5cf7eab7d1eff08f3a7959e5f152086
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-14 19:05:14 +0900

    Add : 기능구현 완료

--- src/pages/PostAdd/PostAdd.js
+++ src/pages/PostAdd/PostAdd.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PostAdd.scss';
+import { useNavigate } from 'react-router-dom';
 
 const PostAdd = () => {
+  const [comment, setComment] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleText = event => {
+    setComment(event.target.value);
+  };
+
+  const cancel = () => {
+    alert('취소');
+    navigate('/post-list');
+  };
+  const posting = () => {
+    if (comment.length >= 1) {
+      navigate('/post-list');
+    } else {
+      alert('내용을 적어주세요.');
+    }
+
+    fetch({
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        title: 'update title',
+        content: '서버에 요청할때 담아서 보는 정보',
+      }),
+    })
+      .then(response => response.json)
+      .then();
+  };
+
   return (
     <div>
       <div className="userBox">
         <div className="userWrap">
-          <img className="user" src="/images/user.png" />
+          <img className="user" src="/images/user.png" alt="adfa" />
           <div className="contentBox">
             <div className="userName">NAME</div>
-            <textarea className="textArea" name="content" cols="80" rows="30">
-              게시물
-            </textarea>
+            <textarea
+              onChange={handleText}
+              className="textArea"
+              cols="80"
+              rows="30"
+              type="text"
+              value={comment}
+            />
+
             <div className="buttonWrap">
-              <button className="cancelButton">취소</button>
-              <button className="addButton">게시</button>
+              <button onClick={cancel} className="cancelButton">
+                취소
+              </button>
+              <button onClick={posting} className="addButton">
+                게시
+              </button>
             </div>
           </div>
         </div>
