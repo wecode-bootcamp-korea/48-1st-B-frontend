0c3f92f8de5be844691b16c6001c1a4d3c4a31e8
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-15 14:58:43 +0900

    Add : 레이아웃,기능 멘토 리뷰 수정 완료

--- src/pages/PostAdd/PostAdd.js
+++ src/pages/PostAdd/PostAdd.js
@@ -1,68 +1,73 @@
 import React, { useState } from 'react';
-import './PostAdd.scss';
 import { useNavigate } from 'react-router-dom';
+import './PostAdd.scss';
 
 const PostAdd = () => {
   const [comment, setComment] = useState('');
 
   const navigate = useNavigate();
 
-  const handleText = event => {
-    setComment(event.target.value);
+  const handleText = value => {
+    setComment(value);
   };
+  console.log(handleText);
 
   const cancel = () => {
-    alert('취소');
-    navigate('/post-list');
+    const confirmed = window.confirm(
+      '적으신 글을 취소하고 post-list로 넘어가겠습니까?',
+    );
+    if (confirmed) {
+      navigate('/post-list');
+    }
   };
+
   const posting = () => {
     if (comment.length >= 1) {
       navigate('/post-list');
-    } else {
-      alert('내용을 적어주세요.');
-    }
 
-    fetch({
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify({
-        title: 'update title',
-        content: '서버에 요청할때 담아서 보는 정보',
-      }),
-    })
-      .then(response => response.json)
-      .then();
+      fetch({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({
+          title: 'update title',
+          content: '서버에 요청할때 담아서 보는 정보',
+        }),
+      })
+        .then(response => response.json)
+        .then(data => console.log(data));
+    } else {
+      alert('내용을 작성주세요.');
+    }
   };
 
   return (
-    <div>
-      <div className="userBox">
-        <div className="userWrap">
-          <img className="user" src="/images/user.png" alt="adfa" />
-          <div className="contentBox">
-            <div className="userName">NAME</div>
-            <textarea
-              onChange={handleText}
-              className="textArea"
-              cols="80"
-              rows="30"
-              type="text"
-              value={comment}
-            />
+    <div className="userBox">
+      <div className="userWrap">
+        <img className="user" src="/images/user.png" alt="userPicture" />
+        <div className="contentBox">
+          <div className="userName">NAME</div>
+          <textarea
+            onChange={event => handleText(event.target.value)}
+            className="textArea"
+            cols="80"
+            rows="30"
+            type="text"
+            value={comment}
+          />
 
-            <div className="buttonWrap">
-              <button onClick={cancel} className="cancelButton">
-                취소
-              </button>
-              <button onClick={posting} className="addButton">
-                게시
-              </button>
-            </div>
+          <div className="buttonWrap">
+            <button onClick={cancel} className="customButton cancelButton">
+              취소
+            </button>
+            <button onClick={posting} className="customButton addButton">
+              게시
+            </button>
           </div>
         </div>
       </div>
+      ç
     </div>
   );
 };
--- src/pages/PostEditing/PostEditing.js
+++ src/pages/PostEditing/PostEditing.js
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './PostEditing.scss';
 
 const PostEditing = () => {
+  const [editComment, setEditComment] = useState('');
+
+  const handleEditText = value => {
+    setEditComment(value);
+  };
+  const navigate = useNavigate();
+
+  const handleEditCancle = () => {
+    const confirmed = window.confirm(
+      '적으신 글을 취소하고 post-list로 넘어가겠습니까?',
+    );
+    if (confirmed) {
+      navigate('/post-list');
+    }
+  };
+
+  const handleEditAdd = () => {
+    if (editComment.length >= 1) {
+      navigate('/post-list');
+
+      fetch({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({
+          title: 'update title',
+          content: '서버에 요청할때 담아서 보는 정보',
+        }),
+      })
+        .then(response => response.json)
+        .then(data => console.log(data));
+    } else {
+      alert('내용을 작성해주세요');
+    }
+  };
   return (
     <div>
-      <div className="commentUserBox">
-        <div className="commentUserWrap">
-          <img className="commentUser" src="/images/user.png" />
-          <div className="commentContentBox">
-            <div className="commentUserName">NAME</div>
+      <div className="userBox">
+        <div className="userWrap">
+          <img className="user" src="/images/user.png" />
+          <div className="contentBox">
+            <div className="userName">NAME</div>
             <textarea
-              className="commentTextArea"
-              name="content"
+              onChange={event => handleEditText(event.target.value)}
+              className="textArea"
+              typeof="text"
               cols="80"
               rows="30"
-            >
-              내용 수정하기
-            </textarea>
-            <div className="commentButtonWrap">
-              <button className="commentCancelButton">취소</button>
-              <button className="commentAddButton">게시</button>
+              value={editComment}
+            />
+            <div className="buttonWrap">
+              <button
+                onClick={handleEditCancle}
+                className="customButton cancelButton"
+              >
+                취소
+              </button>
+              <button
+                onClick={handleEditAdd}
+                className="customButton addButton"
+              >
+                게시
+              </button>
             </div>
           </div>
         </div>
