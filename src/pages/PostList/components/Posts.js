df1083d81f5c18284238efd8240cf0fc5f3a1c5e
Jihye Lee <ehckehck14@gmail.com>
2023-08-16 22:16:28 +0900

    Posts.js 레이아웃 변경 및 scss 추가, .posts에 cusor를 pointer로 변경

--- src/pages/PostList/components/Posts.js
+++ src/pages/PostList/components/Posts.js
@@ -17,36 +17,42 @@ const Posts = ({ postData }) => {
 
     return (
       <div className="post" key={id}>
-        <div className="writerDesktop">
-          <div className="id">
-            <img className="profileThumb" src={userProfile} alt="profile" />
-            <p className="name">{userName}</p>
+        <div className="postAll">
+          <div className="writerDesktop">
+            <div className="id">
+              <img className="profileThumb" src={userProfile} alt="profile" />
+              <p className="name">{userName}</p>
+            </div>
+            <div className="right">
+              <p className="smallFont">{createdAt}</p>
+              <p className="smallFont red">삭제</p>
+              <p className="smallFont black">수정</p>
+            </div>
           </div>
-          <p className="smallFont">{createdAt}</p>
-          <p className="smallFont red">삭제</p>
-          <p className="smallFont black">수정</p>
-        </div>
 
-        <p className="letters">{userLetter}</p>
+          <p className="letters">{userLetter}</p>
 
-        <div className="actionInfo">
-          <div className="info">
-            <p className="smallFont">좋아요{likeCount}</p>
-            <p className="smallFont">댓글{commentCount}</p>
-          </div>
-          <div className="heart">
-            <img src="images/heart.png" alt="heart" />
+          <div className="actionInfo">
+            <div className="info">
+              <p className="smallFont">좋아요{likeCount}</p>
+              <p className="smallFont">댓글{commentCount}</p>
+            </div>
+            <div className="heart">
+              <img src="images/heart.png" alt="heart" />
+            </div>
           </div>
         </div>
-        <div className="replyInput">
-          <input type="text" placeholder="댓글을 작성해주세요" />
-          <button>댓글 게시</button>
-        </div>
-        {comments.length > 0 && (
-          <div className="replyList">
-            <Reply comments={comments} />
+        <div className="reply">
+          <div className="replyInput">
+            <input type="text" placeholder="댓글을 작성해주세요" />
+            <button>댓글 게시</button>
           </div>
-        )}
+          {comments.length > 0 && (
+            <div className="replyList">
+              <Reply comments={comments} />
+            </div>
+          )}
+        </div>
       </div>
     );
   });
