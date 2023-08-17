ecdcbdda55acb7e8f937aea11bfa00a991a90e81
Jihye Lee <ehckehck14@gmail.com>
2023-08-17 13:00:56 +0900

    Modify : Reply.js에서도 시간형식 변경, scss 수정

--- src/pages/PostList/components/Reply.js
+++ src/pages/PostList/components/Reply.js
@@ -5,6 +5,15 @@ const Reply = ({ comments }) => {
   return comments.map(reply => {
     const { commentId, profileImage, userName, comment, createdAt } = reply;
 
+    const dateChange = today => {
+      const year = String(today.getFullYear()).slice(-2);
+      const month = today.getMonth();
+      const date = today.getDate();
+      const yearMonthDate = `${year}-${month}-${date}`;
+
+      return yearMonthDate;
+    };
+
     return (
       <div className="replyPost" key={commentId}>
         <div className="left">
@@ -13,10 +22,12 @@ const Reply = ({ comments }) => {
         <div className="right">
           <div className="postHead">
             <p className="name">{userName}</p>
-            <p className="smallFont">{createdAt}</p>
-            <div className="deleteCorrection">
-              <p className="smallFont red">삭제</p>
-              <p className="smallFont black">수정</p>
+            <div className="topRight">
+              <p className="smallFont">{dateChange(new Date(createdAt))}</p>
+              <div className="deleteCorrection">
+                <p className="smallFont red">삭제</p>
+                <p className="smallFont black">수정</p>
+              </div>
             </div>
           </div>
           <p className="replyLetters">{comment}</p>
